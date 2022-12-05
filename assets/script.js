77df40734132be063150967633b24a675362dd1a
Dmitry Usov <dmitry.usov@yahoo.com>
2022-12-05 08:13:49 +0300

    feat: top menu

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
+
 function toggleTopMenu(event) {
     console.log('menu');
-    document.getElementsByClassName('nav-top')[0].classList.toggle('open');
+
+    document.getElementsByClassName('nav-overlay')[0].classList.toggle('open-top-menu');
+    document.getElementsByClassName('nav-container')[0].classList.toggle('open-top-menu');
+    document.getElementsByClassName('nav-top')[0].classList.toggle('open-top-menu');
+    document.getElementsByClassName('nav-top')[0].querySelector('ul').classList.toggle('open-top-menu');
+    document.getElementById('cover').classList.toggle('covering');
 }
 
 document.getElementsByClassName('toggle-nav-top')[0].addEventListener("click", toggleTopMenu);
+document.getElementById('cover').addEventListener("click", toggleTopMenu);
