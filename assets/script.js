099c19db85996dbc2b7aca2dfdaac838ac1bc251
Dmitry Usov <dmitry.usov@yahoo.com>
2022-12-05 10:57:13 +0300

    feat: carousel animals

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
-
+const animalsInfo = [
+    {
+        img: '../../assets/images/pandas_push_me_bro.jpeg',
+        alt: 'Two Pandas',
+        btn: 'Giant pandas',
+        small: 'Native to Southwest China',
+        icon: 'icon-fruits'
+    }
+]
 function toggleTopMenu(event) {
-    console.log('menu');
 
     document.getElementsByClassName('nav-overlay')[0].classList.toggle('open-top-menu');
     document.getElementsByClassName('nav-container')[0].classList.toggle('open-top-menu');
@@ -9,5 +16,12 @@ function toggleTopMenu(event) {
     document.getElementById('cover').classList.toggle('covering');
 }
 
+const slideAnimals = (event) => {
+    console.log('Slide animals', event.srcElement.id);
+    document.getElementsByClassName('animals-card')[0].scrollBy(200,0);
+}
+
 document.getElementsByClassName('toggle-nav-top')[0].addEventListener("click", toggleTopMenu);
 document.getElementById('cover').addEventListener("click", toggleTopMenu);
+document.getElementById('button-arrow-left').addEventListener("click", slideAnimals);
+document.getElementById('button-arrow-right').addEventListener("click", slideAnimals);
